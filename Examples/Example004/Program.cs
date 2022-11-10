512c2b6ec61773f38c09f93dc8356f103639fb30
Aleksandr <lepalovskiy2015@mail.ru>
2022-11-10 15:42:05 +0700

    003,1

--- Examples/Example004/Program.cs
+++ Examples/Example004/Program.cs
@@ -1,4 +1,4 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1, 10); //число от 1 до 9
+double numberB = new Random().Next(1, 10);
 Console.WriteLine (numberA / numberB);
 
